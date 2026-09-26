1e0595544e5e73062ba9ee2591ca747293397f85
xatusbetazx17 <xatusbetazx17@gmail.com>
2026-09-26 13:30:28 -0400

    Use LiquidAI export with standard CPU embedding operators

--- mobile/src/inference.js
+++ mobile/src/inference.js
@@ -1,7 +1,7 @@
 import {pipeline,env,TextStreamer} from '@huggingface/transformers';
 import {modelCache} from './model-cache.js';
 env.useCustomCache=true;env.customCache=modelCache;
-const MODEL='onnx-community/LFM2.5-350M-ONNX',REV='2c07371c2e84776cad597f3d813b7d306d292aea';
+const MODEL='onnx-community/LFM2-350M-ONNX',REV='5bc4b3e8cfd21660c0b1b9faa447ffbd9926b829';
 env.allowLocalModels=false;env.useBrowserCache=true;env.backends.onnx.wasm.numThreads=1;env.backends.onnx.wasm.proxy=false;env.backends.onnx.wasm.wasmPaths=new URL('./ort/',self.location).href;
 let generator=null,busy=false;
 self.onmessage=async({data})=>{
@@ -10,8 +10,8 @@ self.onmessage=async({data})=>{
  try{
   if(data.action==='load'){
    env.allowRemoteModels=true;
-   generator=await pipeline('text-generation',MODEL,{revision:REV,dtype:'q8',device:'wasm',progress_callback:x=>self.postMessage({type:'progress',message:x.status+(x.progress?' '+Math.round(x.progress)+'%':'')})});
-   for(const file of ['model_quantized.onnx','model_quantized.onnx_data']){
+   generator=await pipeline('text-generation',MODEL,{revision:REV,dtype:'q4',device:'wasm',progress_callback:x=>self.postMessage({type:'progress',message:x.status+(x.progress?' '+Math.round(x.progress)+'%':'')})});
+   for(const file of ['model_q4.onnx','model_q4.onnx_data']){
    const cached=await modelCache.match('https://huggingface.co/'+MODEL+'/resolve/'+REV+'/onnx/'+file);
    if(!cached)throw Error('The model ran but could not be saved offline. Free browser storage and download it again.');
    await cached.body.cancel();
